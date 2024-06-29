c0586ba4e21e4eee63d1436ec0f96adecbd54700
Zavier Chambers <144927007+ZavierChambers@users.noreply.github.com>
2024-06-29 11:31:04 -0400

    Add files via upload

---
+++ Active_Directory-Projects/Create_CSV_Of_All_ADUsers.ps1
@@ -0,0 +1,26 @@
+﻿######################################################
+#             Zavier Chambers - 6/29/24              #
+#                                                    #
+#       Creates a CSV, with all Users in AD          #
+#       The status returned can be modified below    #
+#                                                    #
+######################################################
+
+#File path to where you store your AD information
+$filePath = "C:/Full_ADUsers_List.csv"
+
+#holds all ADUsers with all there properties if needed
+$ADUsers = Get-ADUser -Filter * -Properties *
+#loops through each user in AD and creates a custom object to use as an output to the file path above.
+foreach ($user in $ADUsers)
+{
+   #Custom Object used to store the current users info
+   [PSCustomObject]@{
+        Name = $user.name
+        Samaccountname = $user.SamAccountName
+        Enabled = $user.Enabled
+        dateCreated = $user.whenCreated
+
+    } | export-csv -Path $filePath -Delimiter "," -NoTypeInformation -Append
+}
+
